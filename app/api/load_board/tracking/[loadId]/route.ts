706d969a15e06fb6795fc904927186c2b5fde6ef
Luu Hung <73063097+kendyluu810@users.noreply.github.com>
2025-08-23 16:14:20 +0700

    fix: update parameter destructuring in GET function for clarity

--- app/api/load_board/tracking/[loadId]/route.ts
+++ app/api/load_board/tracking/[loadId]/route.ts
@@ -4,11 +4,11 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   req: Request,
-  { params }: { params: { loadId: string } }
+  context: { params: { loadId: string } }
 ) {
   await dbConnect();
 
-  const { loadId } = params;
+  const { loadId } = context.params;
 
   // 1. Find Load by load_id
   const load = await Load.findOne({ load_id: loadId })
